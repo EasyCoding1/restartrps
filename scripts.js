d631a05bb325a9e6daefeba12409bf7fbc6dcc74
easycoding1 <tsjr96@gmail.com>
2023-03-13 15:59:19 +0000

    Logs game result. Next add score tracker.

--- scripts.js
+++ scripts.js
@@ -1,75 +1,88 @@
-
-/*Find player choice variable, trigger computer choice and log player choice to Gamelog */
-const playerOptions = document.querySelectorAll('.pChoice');
-let playerOption = null;
-
-function getPlayerChoice() {
-    playerOptions.forEach(Option => {
-      Option.addEventListener('click', e => {
-        playerOption = e.target.id;
-        console.log('You have chosen ' + playerOption + '!');
-        runComputerChoice ();
-        const cpTextBox = document.querySelector('.gameLogTL');
-        cpTextBox.textContent = 'You pick  ' + playerOption + '!';
-      });
-    });
-    return playerOption;
-  }
-
-let playerChoice = getPlayerChoice();
-
-
-
-
-
-
-
-
-/*Find Computer Choice Variable each time getPlayerChoice is called, and log to gameLog*/
-
-function runComputerChoice() {
-
-    function getComputerChoice() {
-      let cptChoice;
-        let computerOption = Math.floor(Math.random() * 3);
-        if (computerOption === 0) {
-          cptChoice = 'Earth';
-        } else if (computerOption === 1) {
-          cptChoice = 'Wind';
-        } else if (computerOption === 2){
-          cptChoice = 'Fire';
-        }
-        return cptChoice;
-    };
-
-    let computerChoice = getComputerChoice();
-    console.log('Computer picks : ' + computerChoice + '!');
-    const cpTextBox = document.querySelector('.gameLogTR')
-    cpTextBox.textContent = 'Maurice White picks ' + computerChoice + '!';
-  }
-
-
 function playRound () {
-  let result;
-  const playerChoice = getPlayerChoice();
-  const cptChoice = runComputerChoice();
 
-    function determineWinner (playerChoice, cptChoice) {
-      if (playerChoice == cptChoice) {  
-      result = 1; /*draw*/
-      } else if (playerChoice == 'Earth' && cptChoice == 'Fire') {
-        result = 2; /*player win*/
-      } else if (playerChoice == 'Wind' && cptChoice == 'Earth') {
-        result = 2; /*player win*/
-      } else { 
-        result = 3; /*pc win*/
-      }  
-      console.log(result);
+    const playerOptions = document.querySelectorAll('.pChoice');
+    let playerChoice = null;
+    let cptChoice = null;
 
-    };
-    let roundResult = determineWinner(playerChoice, cptChoice);
-    const gameLogBT = document.querySelector('.gameLogB');
-    gameLogBT.textContent = roundResult;
-  }
-  playRound();
-  
+
+    /*Find player choice variable, trigger computer choice and log player choice to Gamelog */
+    function getPlayerChoice() {
+        playerOptions.forEach(Option => {
+          Option.addEventListener('click', e => {
+            playerChoice = e.target.id;
+            console.log(`You have chosen ${playerChoice}!`);
+            runComputerChoice ();
+          });
+        });
+      }
+
+
+
+    /*Find Computer Choice Variable each time getPlayerChoice is called, and log to gameLog*/
+
+    function runComputerChoice(){
+      let computerOption = Math.floor(Math.random() * 3);
+      
+
+      if (computerOption === 0) {
+        cptChoice = 'Earth';
+      } else if (computerOption === 1) {
+        cptChoice = 'Wind';
+      } else {
+        cptChoice = 'Fire';
+      }
+
+      console.log(`Computer picks ${cptChoice}!`)
+      determineWinner();
+
+    }
+
+
+        function determineWinner () {
+          let result = null;
+
+          if (playerChoice === cptChoice) {  
+          result = 1; /*draw*/
+          } else if ( (playerChoice === 'Earth' && cptChoice === 'Fire') ||
+                      (playerChoice === 'Wind' && cptChoice === 'Earth') ||
+                      (playerChoice === 'Fire' && cptChoice === 'Wind')) {
+          result = 2; // player wins
+          } else { 
+            result = 3; /*pc win*/
+          }  
+          
+          console.log(`Result ${result}`);
+          updateGameLog(result);
+        };
+
+
+        function updateGameLog(result) {
+          let playerScore = 0;
+          let computerScore = 0;
+          const playerTextBox = document.querySelector('.gameLogTL');
+          playerTextBox.textContent = `You picked ${playerChoice}!`;
+          const computerTextBox = document.querySelector('.gameLogTR');
+          computerTextBox.textContent = `Maurice White picked ${cptChoice}!`;
+          const resultTextBox = document.querySelector('.gameLogB');
+          
+          switch (result) {
+            case 1:
+              resultTextBox.textContent = 'It is a draw!';
+              break;
+            case 2:
+              resultTextBox.textContent = 'You win!';
+              playerScore++;
+              console.log('Player Score: ' + playerScore);
+              break;
+            case 3:
+              resultTextBox.textContent = 'Maurice White wins!';
+              computerScore++;
+              console.log('Computer Score : ' + computerScore);
+              break;
+          }
+        }
+        
+        getPlayerChoice();
+      }
+      
+      playRound();
