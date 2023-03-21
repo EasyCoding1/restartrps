3e1765eaf16d18938bc28fbc233cb70ef7f99fcb
easycoding1 <tsjr96@gmail.com>
2023-03-21 16:49:17 +0000

    Added music and sound toggle.

--- scripts.js
+++ scripts.js
@@ -1,118 +1,133 @@
-const startGameBtn = document.querySelector('#start-game-btn');
+// Selecting DOM elements
+const resetGameBtn = document.querySelector('#start-game-btn');
+const playerOptions = document.querySelectorAll('.pChoice');
+const playerScoreBox = document.querySelector('.playerScore');
+const computerScoreBox = document.querySelector('.computerScore');
+const playerTextBox = document.querySelector('.gameLogTL');
+const computerTextBox = document.querySelector('.gameLogTR');
+const resultTextBox = document.querySelector('.gameLogB');
 
-startGameBtn.addEventListener('click', () => {
-  playRound();
+let playerScore = 0;
+let computerScore = 0;
+
+// Reset game function
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  updateGameLog(0, undefined, undefined);
+}
+
+// Bind event listener to the Reset Game button
+resetGameBtn.addEventListener('click', () => {
+  resetGame();
+});
+
+// Bind event listeners to player choice buttons
+playerOptions.forEach(option => {
+  option.addEventListener('click', e => {
+    const playerChoice = e.currentTarget.id;
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    updateGameLog(result, playerChoice, computerChoice);
+  });
 });
 
 
+function getComputerChoice() {
+  const computerOption = Math.floor(Math.random() * 3);
 
-
-
-function playRound () {
-  const playerOptions = document.querySelectorAll('.pChoice');
-  let playerChoice = null;
-  let cptChoice = null;
-  let playerScore = 0;
-  let computerScore = 0;
-
-  function getPlayerChoice() {
-    playerOptions.forEach(Option => {
-      Option.addEventListener('click', e => {
-        playerChoice = e.target.id;
-        console.log(`You have chosen ${playerChoice}!`);
-        runComputerChoice ();
-      });
-    });
+  if (computerOption === 0) {
+    return 'Earth';
+  } else if (computerOption === 1) {
+    return 'Wind';
+  } else {
+    return 'Fire';
   }
-
-  function runComputerChoice(){
-    let computerOption = Math.floor(Math.random() * 3);
-
-    if (computerOption === 0) {
-      cptChoice = 'Earth';
-    } else if (computerOption === 1) {
-      cptChoice = 'Wind';
-    } else {
-      cptChoice = 'Fire';
-    }
-
-    console.log(`Computer picks ${cptChoice}!`)
-    determineWinner();
-  }
-
-  function determineWinner () {
-    let result = null;
-
-    if (playerChoice === cptChoice) {  
-      result = 1; /*draw*/
-    } else if ((playerChoice === 'Earth' && cptChoice === 'Fire') ||
-               (playerChoice === 'Wind' && cptChoice === 'Earth') ||
-               (playerChoice === 'Fire' && cptChoice === 'Wind')) {
-      result = 2; // player wins
-    } else { 
-      result = 3; /*pc win*/
-    }  
-    
-    console.log(`Result ${result}`);
-    updateGameLog(result);
-    changeScore(result);
-  }
-
-  function updateGameLog(result) {
-    const playerTextBox = document.querySelector('.gameLogTL');
-    playerTextBox.textContent = `You picked ${playerChoice}!`;
-    const computerTextBox = document.querySelector('.gameLogTR');
-    computerTextBox.textContent = `Maurice White picked ${cptChoice}!`;
-    const resultTextBox = document.querySelector('.gameLogB');
-          
-    switch (result) {
-      case 1:
-        resultTextBox.textContent = 'It is a draw!';
-        break;
-      case 2:
-        resultTextBox.textContent = 'You win!';
-        break;
-      case 3:
-        resultTextBox.textContent = 'Maurice White wins!';
-        break;
-    }
-
-    const playerScoreBox = document.querySelector('.playerScore');
-    const computerScoreBox = document.querySelector('.computerScore');
-
-    playerScoreBox.textContent = `Player Score: ${playerScore}`;
-    computerScoreBox.textContent = `Computer Score: ${computerScore}`;
-  }
-
-  function changeScore(result) {
-    if (result === 1) {
-      playerScore++;
-    } else if (result === 2) {
-      computerScore++;
-    }
-
-    console.log(`Player Score is ${playerScore}`);
-    console.log(`Computer Score is ${computerScore}`);
-
-    const playerScoreBox = document.querySelector('.playerScore');
-    const computerScoreBox = document.querySelector('.computerScore');
-
-    playerScoreBox.textContent = `Player Score: ${playerScore}`;
-    computerScoreBox.textContent = `Computer Score: ${computerScore}`;
-
-    if (playerScore === 5 || computerScore === 5) {
-      const playAgain = confirm(`Game Over! Do you want to play again?`);
-
-      if (playAgain) {
-        playerScore = 0;
-        computerScore = 0;
-        updateGameLog(0);
-        getPlayerChoice();
-      } else {
-        alert(`Thanks for playing!`);
-      }
-    }
-  }
-
-  getPlayerChoice();
 }
+
+function determineWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return 1; // draw
+  } else if (
+    (playerChoice === 'Earth' && computerChoice === 'Fire') ||
+    (playerChoice === 'Wind' && computerChoice === 'Earth') ||
+    (playerChoice === 'Fire' && computerChoice === 'Wind')
+  ) {
+    playerScore++;
+    updateScore(playerScoreBox);
+    return 2; // player wins
+  } else {
+    computerScore++;
+    updateScore(computerScoreBox);
+    return 3; // computer wins
+  }
+}
+
+function updateScore(scoreElement) {
+  scoreElement.classList.add("score-update");
+  setTimeout(() => {
+    scoreElement.classList.remove("score-update");
+  }, 1000);
+}
+
+function updateGameLog(result, playerChoice, computerChoice) {
+  playerTextBox.textContent = `You picked ${playerChoice}!`;
+  computerTextBox.textContent = `Maurice White picked ${computerChoice}!`;
+
+  switch (result) {
+    case 1:
+      resultTextBox.textContent = 'It is a draw!';
+      break;
+    case 2:
+      resultTextBox.textContent = 'You win!';
+      indicateWinner(playerScoreBox);
+      break;
+    case 3:
+      resultTextBox.textContent = 'Maurice White wins!';
+      indicateWinner(computerScoreBox);
+      break;
+    default:
+      resultTextBox.textContent = '';
+      break;
+  }
+
+  playerScoreBox.textContent = `Player Score: ${playerScore}`;
+  computerScoreBox.textContent = `Computer Score: ${computerScore}`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    const playAgain = confirm(`Game Over! Do you want to play again?`);
+
+    if (playAgain) {
+      resetGame();
+    } else {
+      alert(`Thanks for playing!`);
+    }
+  }
+}
+
+function indicateWinner(winnerElement) {
+  winnerElement.classList.add("win-indicator");
+  setTimeout(() => {
+    winnerElement.classList.remove("win-indicator");
+  }, 1000);
+}
+
+
+const soundButton = document.querySelector('#soundTog');
+const backgroundMusic = document.querySelector('#background-music');
+
+
+
+backgroundMusic.play();
+
+// Toggle the audio on and off when the sound button is clicked
+soundButton.addEventListener("click", () => {
+  if (backgroundMusic.paused) {
+    backgroundMusic.play();
+    soundButton.textContent = "Mute"
+  } else {
+    backgroundMusic.pause();
+    soundButton.textContent = "Sound"
+  }
+});
+
